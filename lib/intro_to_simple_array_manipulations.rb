749a47018ff3d77cbf2e80b22a96aa8776b56836
Kizito Njoku <nkizito57@gmail.com>
2019-10-15 15:48:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -40,7 +40,11 @@ def using_flatten(array)
   
   array.flatten
 end
-def using_flatten(array) 
+def using_delete(array, string) 
   
-  array.flatten
+  array.delete(string)
+end
+def using_delete(array, string) 
+  
+  array.delete(string)
 end
