5ac7ac0f4cfd48f45b33bab7fc5eaaadfc719950
Kizito Njoku <nkizito57@gmail.com>
2019-10-15 15:44:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,30 @@
 def using_push(array, string)
-
   array.push(string)
 end
 
 
-def unshift(array, string)
-  string = "Hopfull"
+def using_unshift(array, string)
+  
   array.unshift(string)
+end
+def using_shift(array)
+  
+  array.shift
+end
+def shift_with_args(array)
+  
+  array.shift(2)
+end
+
+def using_pop(array)
+  
+  array.pop
+end
+def pop_with_args(array)
+  
+  array.pop(2)
+end
+def using_concat(array, arr)
+  
+  array.concat(arr)
 end
